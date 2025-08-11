b33b24409b13cc7553ac632dc39248f5ab0cfa7e
caiyancheng <19307140030@fudan.edu.cn>
2025-08-11 02:08:42 +0100

    2025-8-11

--- Train_on_ImageNet/run_auto.sh
+++ Train_on_ImageNet/run_auto.sh
@@ -6,5 +6,11 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate hvs_2
 cd /auto/homes/yc613/Py_codes/Why_NN_HVS/Train_on_ImageNet
 
-python train_main.py --model mobilenet_v3_large --output ./runs_mobilenet_v3_large
+export CUDA_VISIBLE_DEVICES=0,1
+torchrun --nproc_per_node=2 --master_port=29501 \
+  /auto/homes/yc613/Py_codes/Why_NN_HVS/Train_on_ImageNet/train_main.py \
+  --train-dir /local/scratch-2/yc613/Datasets/ILSVRC2012_img_train \
+  --val-dir   /local/scratch-2/yc613/Datasets/ILSVRC2012_img_val \
+  --model mobilenet_v3_large --epochs 60 --batch-size 256 --amp --channels-last \
+  --output ./runs_mobilenet_v3_large
 
