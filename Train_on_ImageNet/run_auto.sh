a7a0417f72d3762f54a36c3578911f6456bccde0
caiyancheng <19307140030@fudan.edu.cn>
2025-08-11 01:59:09 +0100

    2025-8-11

--- Train_on_ImageNet/run_auto.sh
+++ Train_on_ImageNet/run_auto.sh
@@ -4,7 +4,7 @@
 # 注意：激活conda环境要用source并指定conda.sh路径
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate hvs_2
-cd /auto/homes/yc613/Py_codes/Train_on_ImageNet
+cd /auto/homes/yc613/Py_codes/Why_NN_HVS/Train_on_ImageNet
 
 python train_main.py --model mobilenet_v3_large --output ./runs_mobilenet_v3_large
 
